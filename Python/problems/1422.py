bd63e0166cc50d3a4d410d1dc8e85a66de568f14
Roushan Singh <github.rtron18@gmail.com>
2025-01-05 17:41:10 +0530

    Add comments to Python/problems/1422.py

--- Python/problems/1422.py
+++ Python/problems/1422.py
@@ -1,12 +1,19 @@
 class Solution:
     def maxScore(self, s: str) -> int:
+        # Count of '0's in the left substring
         zero = 0
+
+        # count of '1's in the right substring
         ones = s.count("1")
+
         ans = 0
         for i in range(len(s) - 1):
             if s[i] == "0":
-                zero += 1
+                # Increment '0' count for the left substring
+                zero += 1  
             else:
-                ones -= 1
+                # Decrement '1' count for the right substring
+                ones -= 1  
+
             ans = max(ans, zero + ones)
         return ans
